ee77d59ca76f42d678db75f2c22e76f6e7433677
chaoyangrunning <chaoyangrunning@163.com>
2016-11-08 00:14:09 +0800

    封装NSManagedObjectContext类

---
+++ CoreDataDemo/CoreDataDemo/DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  CoreDataDemo
+//
+//  Created by dontbeditouzu on 16/11/8.
+//  Copyright © 2016年 dontbeditouzu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@end
---
+++ CoreDataDemo/CoreDataDemo/DBManager.m
@@ -0,0 +1,13 @@
+//
+//  DBManager.m
+//  CoreDataDemo
+//
+//  Created by dontbeditouzu on 16/11/8.
+//  Copyright © 2016年 dontbeditouzu. All rights reserved.
+//
+
+#import "DBManager.h"
+
+@implementation DBManager
+
+@end
