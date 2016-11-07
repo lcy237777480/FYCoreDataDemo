35900f66c599f9a468b16e45dc877ade6d0e5c72
chaoyangrunning <chaoyangrunning@163.com>
2016-11-07 23:52:43 +0800

    创建项目

---
+++ CoreDataDemo/CoreDataDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataDemo
+//
+//  Created by dontbeditouzu on 16/11/7.
+//  Copyright © 2016年 dontbeditouzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreDataDemo/CoreDataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataDemo
+//
+//  Created by dontbeditouzu on 16/11/7.
+//  Copyright © 2016年 dontbeditouzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
