bf28b3c95d49bb00caa06533e4de90b4f61413f8
chaoyangrunning <chaoyangrunning@163.com>
2016-11-08 00:43:19 +0800

    微调

--- CoreDataDemo/CoreDataDemo/DBManager.h
+++ CoreDataDemo/CoreDataDemo/DBManager.h
@@ -18,14 +18,15 @@
 // 增加数据
 -(void)insert:(NSString *)name sex:(NSString *)sex age:(NSNumber *)age uid:(NSString *)uid;
 
-// 排序和筛选
--(NSArray *)fiterSex:(NSString *)sex ageAcsending:(NSComparisonResult)compare;
-
 // 删除
 -(void)deletePeopleWithUid:(NSString *)uid;
 
 // 更改
 -(void)updatePeople:(People *)people name:(NSString *)name age:(NSNumber *)age sex:(NSString *)sex;
+
+// 排序和筛选
+-(NSArray *)fiterSex:(NSString *)sex ageAcsending:(NSComparisonResult)compare;
+
 
 // 按性别筛选
 -(NSArray *)filterSex:(NSString *)sex;
